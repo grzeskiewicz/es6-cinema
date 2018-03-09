b260fe20497a962605b808c6136e45685a664a5b
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-03-09 10:41:25 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,174 @@
+import { authServices } from './services.js';
+
+
+Handlebars.registerHelper('for', function(from, to, block) {
+    var accum = '';
+    for (var i = from; i <= to; i++)
+        accum += block.fn(i);
+    return accum;
+});
+
+
+const listService = { //same object like picedSeats, maybe Object.create(pattern)?
+    elemArray: [],
+    selectedElem: undefined,
+    add(elem) {
+        this.elemArray.push(elem);
+    },
+    addNoRepeat(elem) {
+        let index = elemArray.indexOf(elem);
+        (index >= 0) ? this.elemArray.splice(index, 1): this.elemArray.push(elem);
+
+    },
+    selectById(id) {
+    this.selectedElem = this.elemArray[0].showings.find(element => element.id === Number(id));
+    },
+    list() { return this.elemArray; },
+    first() { return this.elemArray[0] },
+    remove() { this.elemArray = []; },
+    getSelected(){ return this.selectedElem}
+}
+const showingsService = Object.create(listService);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// MAIN PART =======================================================================================================================================================
+
+
+
+
+//1: const API_URL = 'https://api.myjson.com/bins/17jq6h';
+//const API_URL='https://api.myjson.com/bins/vidvd';
+const API_URL = 'https://api.myjson.com/bins/w7wup';
+//const API_URL = 'https://api.myjson.com/bins/5yuh5';
+//const API_URL = 'http://localhost:8000';
+//const API_URL = 'http://localhost:3000/showings';
+const headers = new Headers({
+    'Access-Control-Allow-Origin': '*'
+});
+
+
+function request(url, method, data) {
+    return new Request(url, {
+        method: method,
+        mode: 'cors',
+        header: headers,
+        body: data
+    });
+}
+
+fetch(request(API_URL, 'GET'))
+    .then(res => res.json())
+    .then(showings => {
+        showingsCtrl.list(showings);
+        seatsCtrl.toggleListener();
+        showingsService.add(showings);
+    });
+
+
+
+const view = {
+    renderContent(source, context, output) {
+        let sourceElem = document.getElementById(source).innerHTML;
+        let template = Handlebars.compile(sourceElem);
+        let html = template(context);
+        document.getElementById(output).innerHTML = html;
+    }
+}
+
+const showingsCtrl = {
+    showingsList: document.getElementsByTagName("a"),
+    list(showings) {
+        view.renderContent("entry-template", showings, "showings");
+        [...this.showingsList].forEach(showing => {
+            showing.addEventListener('click', event => {
+
+                event.preventDefault();
+                view.renderContent("entry-template-seats", event.currentTarget.dataset, "seats");
+                seatsCtrl.selectedSeats = [];
+                document.getElementById("order").innerHTML="";
+                seatsCtrl.toggleListener();
+                orderCtrl.orderListener();
+                console.log(event.currentTarget.dataset.showingId);
+                showingsService.selectById(event.currentTarget.dataset.showingId);
+            }, false);
+        });
+    }
+
+}
+
+const seatsCtrl = {
+    seats: document.getElementsByClassName("seat"),
+    selectedSeats: [],
+    toggleListener() {
+        [...this.seats].forEach(seat => {
+            seat.addEventListener('click', event => {
+                event.target.classList.toggle('selected');
+                if (event.target.classList.contains('selected')) {
+                    this.selectedSeats.push(event.target.firstChild.data);
+                    //console.log(this.selectedSeats);
+                } else {
+                    let seatToRemoveIndex = this.selectedSeats.findIndex(element => element === event.target.firstChild.data);
+                    this.selectedSeats.splice(seatToRemoveIndex, 1);
+                    //console.log(this.selectedSeats);
+                }
+            });
+        })
+    }
+}
+
+
+
+const orderCtrl = {
+    orderListener() {
+        const orderBtn = document.getElementById("ordx");
+        orderBtn.addEventListener('click', event => {
+            console.log(seatsCtrl.selectedSeats);
+            console.log(showingsService.getSelected());
+            const obj={showing:showingsService.getSelected(), seatsSelected: seatsCtrl.selectedSeats};
+            view.renderContent("entry-template-order",obj,"order");
+            view.renderContent("entry-template-login",obj,"login");
+            view.renderContent("entry-template-register",obj,"register");
+            const loginForm=document.forms['login-form'];
+            loginForm.addEventListener('submit',login,false);
+            const registerForm=document.forms['register-form'];
+            console.log("Log");
+            registerForm.addEventListener('submit',registerCtrl.signup,false);
+        });
+    }
+}
+
+
+const registerCtrl = {
+    user: {
+        email: '',
+        password: '',
+        name: '',
+        surename: '',
+        telephone: ''
+    },
+    signup(event) {
+        event.preventDefault();
+        console.log("Heheszki");
+        /*authServies.register(this.user).then(msg => {
+            //$state.go('login');
+            console.log(`Registration msg ${msg}`);
+        }, errMsg => {
+            //fails
+        });*/
+    }
+}
---
+++ services.js
@@ -0,0 +1,229 @@
+/*const seatServices = {
+    pickedSeats: { //testable
+        pickedSeats: [],
+        add(seat) {
+            let index = pickedSeats.indexOf(seat);
+            (index >= 0) ? this.pickedSeats.splice(index, 1): this.pickedSeats.push(seat);
+
+        },
+        list() { return this.pickedSeats; },
+        seat() { return this.pickedSeats[0] },
+        remove() { this.pickedSeats = []; }
+    },
+    seatsTaken: { //same object like picedSeats, maybe Object.create(pattern)?
+        seatsTaken: [],
+        add(arr) {
+            this.seatsTaken.push(seat);
+        },
+        list() { return this.seatsTaken; },
+        seat() { return this.seatsTaken[0] },
+        remove() { this.seatsTaken = []; }
+    },
+
+}
+
+
+const listServices = {
+    listService: { //same object like picedSeats, maybe Object.create(pattern)?
+        elemArray: [],
+        add(elem) {
+            this.elemArray.push(elem);
+        },        
+        addNoRepeat(elem) {
+            let index = elemArray.indexOf(elem);
+            (index >= 0) ? this.elemArray.splice(index, 1): this.elemArray.push(elem);
+
+        },
+        list() { return this.elemArray; },
+        first() { return this.elemArray[0] },
+        remove() { this.elemArray = []; }
+    }
+
+}*/
+
+
+
+
+
+
+export const authServices = {
+    LOCAL_TOKEN_KEY: 'yourTokenKey',
+    isAuthenticated: false,
+    authToken: undefined,
+
+    loadUserCredentials() {
+        var token = window.localStorage.getItem(LOCAL_TOKEN_KEY);
+        if (token) {
+            useCredentials(token);
+        }
+    },
+
+    storeUserCredentials(token) {
+        window.localStorage.setItem(LOCAL_TOKEN_KEY, token);
+        useCredentials(token);
+    },
+
+    useCredentials(token) {
+        isAuthenticated = true;
+        authToken = token;
+
+        // Set the token as header for your requests!
+        $http.defaults.headers.common.Authorization = authToken;
+    },
+
+    destroyUserCredentials() {
+        authToken = undefined;
+        isAuthenticated = false;
+        $http.defaults.headers.common.Authorization = undefined; //!!!!
+        window.localStorage.removeItem(LOCAL_TOKEN_KEY);
+    },
+
+    register(user) {
+        fetch(request(`${API_URL}/signUp`, 'POST', user))
+            .then(res => res.json())
+            .then(result => {
+                if (result.data.success) { // result.ok?
+                    return result.data.msg;
+                }
+                return Promise.reject(Error('error'))
+            }).catch(error => Promise.reject(Error(error.message)));
+
+
+        /*  return $q(function(resolve, reject) {
+              $http.post(`${{API_URL}}/signUp`, user).then(function(result) {
+                  console.log(result);
+                  if (result.data.success) {
+                      resolve(result.data.msg);
+                  } else {
+                      reject(result.data.msg);
+                  }
+              });
+          });*/
+    },
+
+    login(user) { //token? JWT!
+        fetch(request(`${API_URL}/login`, 'POST', user))
+            .then(res => res.json())
+            .then(result => {
+                if (result.data.success) { // result.ok?
+                    storeUserCredentials(result.data.token);
+                    //console.log(result.data.token);
+                    return result.data.msg;
+                }
+                return Promise.reject(Error('error'))
+            }).catch(error => Promise.reject(Error(error.message)));
+        /*return $q(function(resolve, reject) {
+            $http.post(`${{API_URL}}/login`, user).then(function(result) {
+                if (result.data.success) {
+                    storeUserCredentials(result.data.token);
+                    console.log(result.data.token);
+                    resolve(result.data.msg);
+                } else {
+                    reject(result.data);
+                }
+            });
+        });*/
+    },
+
+    logout() {
+        destroyUserCredentials();
+    }
+
+    //loadUserCredentials();
+
+}
+
+
+
+
+
+
+/*
+const loginCtrl = {
+    user: {
+        email: '',
+        password: ''
+    },
+    register() { $state.go('register'); },
+    login() {
+        AuthService.login(this.user).then(function(msg) {
+            // $rootScope.noshowlogin=true;
+            //$scope.getInfo();
+        }, function(errMsg) {
+            // $scope.error=true;
+            //$scope.errorMsg=errMsg.msg;
+            // if (errMsg.wrongpassword==true) {$scope.errorpassword=true;}
+        });
+    },
+    destrySession() { authServies.logout(); },
+    getInfo() {
+        fetch(`${API_ENDPOINT}/memberinfo`, 'GET').then(function(result) {
+            if (result.data.success) {
+                $rootScope.noshowlogin = true;
+                $scope.memberinfo = result.data.msg;
+                $rootScope.memberinfo2 = result.data.msg;
+                $state.go('order');
+            }
+        });
+
+    },
+    logout() {
+        authServies.logout();
+        //$rootScope.noshowlogin=false;
+        //$state.go('login');
+        //$rootScope.memberinfo2=undefined;
+        //$scope.memberinfo=undefined;
+    }
+
+}
+const registerCtrl = {
+    user: {
+        email: '',
+        password: '',
+        name: '',
+        surename: '',
+        telephone: ''
+    },
+    signup() {
+        authServies.register(this.user).then(msg => {
+            //$state.go('login');
+            console.log(`Registration msg ${msg}`);
+        }, errMsg => {
+            //fails
+        });
+    }
+}
+
+
+
+const orderCtrl2 = {
+    //seats: seatServices.pickedSeats.list(),
+    seats: seatsCtrl.selectedSeats,
+    price: undefined,
+    logout() {
+        authServies.logout();
+    },
+    updatePrice() {
+        if (formobj.singleSelect === "normal") { this.price = showingpicked.normal; }
+        if (formobj.singleSelect === "discount") { this.price = showingpicked.discount; }
+        orderReady = true;
+    },
+    processForm() {
+        let ticket = {
+            showing: $rootScope.showingId, //!!
+            seat: this.seats,
+            price: this.price,
+            email: $rootScope.memberinfo2 //!!
+        };
+        fetch(request(`${API_URL}/newticket`, 'POST', ticket))
+            .then(res => res.json())
+            .then(result => {
+                if (result.data.success) { // result.ok?
+                    return result.data.msg;
+                }
+                return Promise.reject(Error('error'))
+            }).catch(error => Promise.reject(Error(error.message)));
+    }
+}
+
+*/
