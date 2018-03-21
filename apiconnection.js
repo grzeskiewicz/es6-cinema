93db82cb35e05163a4095a39fa4b270e153b9395
Benuch <ariel.grzeskiewicz@gmail.com>
2018-03-21 23:29:01 +0100

    register user

--- apiconnection.js
+++ apiconnection.js
@@ -3,15 +3,16 @@ export const API_URL='https://cinemanode.azurewebsites.net/';
 //const API_URL = 'https://api.myjson.com/bins/w7wup';
 
 const headers = new Headers({
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/x-www-form-urlencoded'
+            'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+            'Access-Control-Allow-Origin': '*'
 });
 
 
 export function request(url, method, dataset) {
 return new Request(url, {
         method: method,
-        header: headers,
+        headers: headers,
         mode : 'cors',
         body: JSON.stringify(dataset)
     });
