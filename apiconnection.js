a0970bbabc6b525068c6b5547249ed7c0d5490f2
Benuch <ariel.grzeskiewicz@gmail.com>
2018-03-22 19:53:49 +0100

    headers

--- apiconnection.js
+++ apiconnection.js
@@ -1,13 +1,15 @@
 //export const API_URL = 'https://cinemadb.000webhostapp.com/hehe/index.php/'; PHP server
-export const API_URL='https://cinemanode.azurewebsites.net/';
+export const API_URL = 'https://cinemanode.azurewebsites.net/';
 //const API_URL = 'https://api.myjson.com/bins/w7wup';
 
 const headers = new Headers({
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
 });
 
 
 export function request(url, method, dataset) {
-return new Request(url, {
+    return new Request(url, {
         method: method,
         headers: headers,
         mode: 'cors',
