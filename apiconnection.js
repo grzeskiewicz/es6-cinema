9236705c1566da0c4c38caa906133f4c6c04669e
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-03-27 22:22:34 +0200

    cors added

--- apiconnection.js
+++ apiconnection.js
@@ -5,7 +5,7 @@ export const API_URL='https://https-cinemanode.193b.starter-ca-central-1.openshi
 //const API_URL = 'https://api.myjson.com/bins/w7wup';
 
 const headers = new Headers({
-    'Accept': 'application/json',
+   // 'Accept': 'application/json',
     //'Content-Type': 'application/json',
    // "Access-Control-Allow-Origin":"*"
 });
