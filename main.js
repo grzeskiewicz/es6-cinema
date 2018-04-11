c044f9c7c9e17a04614be4bb87153dc6e3d7b8d3
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-04-11 22:41:03 +0200

    Date manipulation

--- main.js
+++ main.js
@@ -80,7 +80,10 @@ const showingsCtrl = {
         return moment(stringdate).format("DD.MM.YYYY, hh:mm");
     },
     dateDisplay(showings) {
-        showings=showings.sort((a,b) => a.date -b .date);
+        showings=showings.sort((a,b) => {
+console.log(a.date + " " + b.date);
+            return a.date -b .date;
+        });
         console.log(showings);
         for (const showing of showings) {
           //  showing.date = this.dateParser(showing.date)
