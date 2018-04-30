d37f6ef5684b869905008baf805ff4bc067795cc
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-05-01 00:03:07 +0200

    heroku api

--- apiconnection.js
+++ apiconnection.js
@@ -1,7 +1,7 @@
 //export const API_URL = 'https://cinemadb.000webhostapp.com/hehe/index.php/'; PHP server
 //export const API_URL='https://cinemanode.azurewebsites.net/';
 //export const API_URL='http://cinemanode-cinemanode.193b.starter-ca-central-1.openshiftapps.com/';
-export const API_URL='https://https-cinemanode.193b.starter-ca-central-1.openshiftapps.com/';
+export const API_URL='https://cinema-node.herokuapp.com/';
 //const API_URL = 'https://api.myjson.com/bins/w7wup';
 
 export const headers = new Headers({
