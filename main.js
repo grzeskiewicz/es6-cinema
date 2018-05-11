5ace8768ecfc2effbae9b8ebb5a8423d8f2d8a25
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-05-11 15:15:20 +0200

    CSS flexbox

--- main.js
+++ main.js
@@ -217,7 +217,7 @@ const ticketCtrl = {
                     .then(result => {
                         const seatsDiv = document.getElementById('seats');
                         //view.hide(orderForm);
-                        orderForm.classList.add('ordered');
+                        document.querySelector('#order').classList.add('ordered');
                         orderForm.innerHTML = result.msg;
                         view.hide(seatsDiv);
                         console.log(result);
