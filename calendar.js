f3d3ce405235f7b3119d458b4ff110db2edde0b7
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-05-21 20:38:06 +0200

    adding calendar

--- calendar.js
+++ calendar.js
@@ -41,7 +41,7 @@ export function renderCalendar(calendar) {
             tr.appendChild(td);
             td.addEventListener('click', function() {
                 const pickedDate = new Date(this.dataset.date);
-                 console.log(pickedDate);
+                 console.log(pickedDate.toString());
             });
         }
         // mount table row
