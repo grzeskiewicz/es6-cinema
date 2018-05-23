b510922fe891f44009ee8caa9ee32a96e4773055
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-05-23 09:47:47 +0200

    modyfing calendar

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ export const showingsCtrl = {
         });
         console.log(sList);
         for (const x of sList) {
-            console.log(x);
+            console.log(x.date,this.dateParser(x.date));
             //x.date = this.dateParser(x.date);
         }
         //console.log(showings);
