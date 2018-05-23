1e3b052e0c6be4e1f126b10c7465b43ca0b978cc
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-05-23 16:57:53 +0200

    modyfing calendar

--- main.js
+++ main.js
@@ -94,7 +94,7 @@ export const showingsCtrl = {
         for (var key in obj) filtered.push(obj[key].title); 
             console.log(filtered);
 
-       obj2={};
+       let obj2={};
        for (const show of sList){
         for (const title of filtered) {
             if (show.title==title) obj2[title].push(show);
