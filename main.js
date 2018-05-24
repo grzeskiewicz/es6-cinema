d4d77216cb09861a3164a88ae55c0b2d0b31e655
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-05-24 13:57:10 +0200

    modyfing calendar

--- main.js
+++ main.js
@@ -99,13 +99,14 @@ export const showingsCtrl = {
 
         let obj2 = [];
         for (const title of filtered) {
-            obj2[title.title]=[];
+            console.log(title);
+            obj2[title]=[];
         }
         console.log(obj2);
         for (var j = 0, len = sList.length; j < len; j++) {
             for (var i = 0, len = filtered.length; i < len; i++) {
                // if (obj2[filtered[i]] === undefined) { obj2[filtered[i]] = []; }
-                if (sList[j]['title'] == filtered[i]) { obj2[filtered[i]].push(sList[j]); }
+                //if (sList[j]['title'] == filtered[i]) { obj2[filtered[i]].push(sList[j]); }
                 //console.log(sList[j]['title'], obj2[filtered[i]]['title']);
             }
         }
