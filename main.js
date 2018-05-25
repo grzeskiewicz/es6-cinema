0d926e0aec372b50632542a5b802945f9baad782
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-05-25 15:29:37 +0200

    modyfing calendar

--- main.js
+++ main.js
@@ -131,7 +131,7 @@ export const showingsCtrl = {
         [...this.titlesList()].forEach(title => {
             title.addEventListener('click', function() {
                 console.log(titles[1][title.textContent]);
-                view.renderContent("entry-template", JSON.parse(`{ "showings": ${JSON.stringify(titles[1][title.textContent])}}`), "showList");
+                view.renderContent("entry-template", JSON.parse(`{ "showings": ${JSON.stringify(titles[1][title.textContent])}}`), "showlist");
 
                 [...showingsCtrl.showingsList()].forEach(showing => {
 
