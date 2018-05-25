ef689ca76ff90d5bbb76bf03756eadec365924a1
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-05-25 13:40:43 +0200

    modyfing calendar

--- main.js
+++ main.js
@@ -131,7 +131,7 @@ export const showingsCtrl = {
         // view.renderContent("entry-template", JSON.parse(`{ "showings": ${JSON.stringify(showings)}}`), "showings");
         [...this.titlesList()].forEach(title => {
             title.addEventListener('click', function() {
-                console.log(title.textContent);
+                console.log(titles[1][title.textContent]);
                // console.log(titles[1][title.dataset.title]);
             });
         });
