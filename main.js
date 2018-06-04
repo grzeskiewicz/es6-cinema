6e28c1af39ef6106433d6ed5ed7653759b6df565
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-06-04 11:54:48 +0200

    Editing calendar and CSS

--- main.js
+++ main.js
@@ -175,7 +175,7 @@ export const showingsCtrl = {
         view.renderContent("entry-template-titles", JSON.parse(`{ "showings": ${JSON.stringify(titles[0])}}`), "showings");
         [...this.titlesList()].forEach(title => {
             title.addEventListener('click', function() {
-                view.renderContent("entry-template", JSON.parse(`{ "showings": ${JSON.stringify(titles[1][title.textContent])}}`), "showlist"); //list of hours of selected showing
+                view.renderContent("entry-template-times", JSON.parse(`{ "showings": ${JSON.stringify(titles[1][title.textContent])}}`), "showlist"); //list of hours of selected showing
                 view.renderContent("entry-template-film", JSON.parse(`{ "showings": ${JSON.stringify(titles[1][title.textContent][0])}}`), "film"); //description of the film 
                 [...showingsCtrl.showingsList()].forEach(showing => {
 
