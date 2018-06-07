bab94323076232a4ea1737ef07063a9e6ba074a5
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-06-07 10:38:08 +0200

    Editing calendar and CSS

--- calendar.js
+++ calendar.js
@@ -59,8 +59,8 @@ export function renderCalendar(calendar) {
     calendarDiv.appendChild(p);
     // mount table to container
     calendarDiv.appendChild(table);
-    p2.textContent = '▼';
-    calendarDiv.appendChild(p2);
+    //p2.textContent = '▼';
+    //calendarDiv.appendChild(p2);
 
 
     return table;
