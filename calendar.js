e50ce50381c5843392ea709e77cc9d30df011eff
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-07-26 15:25:48 +0200

    Changing calendar

--- calendar.js
+++ calendar.js
@@ -32,7 +32,8 @@ export function renderCalendar(calendar) {
             td.textContent = day.date.getDate();
             td.dataset.date = day.date;
             if (day.date.getMonth() === today.getMonth() && day.date.getDate() === today.getDate()) td.classList.add('today');
-
+            if (day.date < today ) { td.classList.add('not-selectable'); }
+            if (td.classList.contains('today')) td.classList.remove('not-selectable');
             // add before/after class
             day.before && td.classList.add('before');
             day.after && td.classList.add('after');
