eef4ad5d20e436b89325e2b86dc519fca92aa330
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-08-27 23:39:20 +0200

    Editing whole

--- main.js
+++ main.js
@@ -147,7 +147,7 @@ const calendarCtrl = {
                     const showingsWrapper = document.querySelector('#showings-wrapper');
                     const pickedDate = new Date(this.dataset.date);
                     view.hide(showlist);
-                    view.hide(details);
+                    //view.hide(details);
                     const showings = showingsCtrl.calendarShowings(pickedDate);
                     showlist.innerHTML = '';
                    // seats.innerHTML = '';
