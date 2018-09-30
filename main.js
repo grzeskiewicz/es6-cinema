ba26f138abd322701246136553299deb8ef9e1a9
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-09-30 17:31:25 +0200

    Editing calendar

--- main.js
+++ main.js
@@ -278,6 +278,7 @@ export const showingsCtrl = {
                         orderCtrl.orderListener();
                         const nextBtn = document.getElementById("nextBtn");
                         view.hide(nextBtn);
+                        console.log(showignsCtrl.goBackToTitlesBtn);
                         showingsCtrl.goBackToTitlesBtn.addEventListener('click', goBackToTitles);
 
                     }, false);
