1bf05bc63eb18dc875d1decc78be6b861043b7bb
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-09-30 18:14:24 +0200

    Editing calendar

--- main.js
+++ main.js
@@ -211,7 +211,7 @@ export const showingsCtrl = {
     goBackToTitles() {
         view.showFlex(showingsCtrl.showList);
         view.showFlex(showingsCtrl.filmTitles);
-        view.hide(this.goBackToTitlesBtn());
+        view.hide(showingsCtrl.goBackToTitlesBtn());
     },
     list(showings) {
         this.sortShowings(showings);
