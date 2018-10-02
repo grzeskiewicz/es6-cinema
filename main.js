66d15ac1ab60e57cfd563111f9043b622cd11685
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-10-02 12:22:45 +0200

    Editing project

--- main.js
+++ main.js
@@ -353,11 +353,7 @@ const orderCtrl = {
 
             //obj.showing.date=moment(obj.showing.date);
             view.renderContent("entry-template-order", obj, "order"); //only valid usage of renderContent
-           // view.renderContent("entry-template-login", obj, "login"); //only form
-           // view.renderContent("entry-template-register", obj, "register"); //only form
 
-           // registerCtrl.registerForm().addEventListener('submit', registerCtrl.signup, false);
-           // loginCtrl.loginForm().addEventListener('submit', loginCtrl.login, false);
 
             const orderForm = document.forms['order-form'];
             document.querySelector('#total-price').innerHTML = `Total price to pay: ${this.pricing()}`;
@@ -383,6 +379,11 @@ const ticketCtrl = {
             if (email === undefined) {
                 document.getElementById("order-status").innerHTML = "Please login to order tickets!";
                 view.hide(seatsCtrl.seatsDiv);
+                         view.renderContent("entry-template-login", obj, "login"); //only form
+           view.renderContent("entry-template-register", obj, "register"); //only form
+
+           registerCtrl.registerForm().addEventListener('submit', registerCtrl.signup, false);
+         loginCtrl.loginForm().addEventListener('submit', loginCtrl.login, false);
 
             } else {
                 const ticket = {
