6a77d5b1c2538909e2bdf36135870ffd50467f11
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-10-05 20:03:00 +0200

    Editing project

--- main.js
+++ main.js
@@ -130,6 +130,7 @@ const calendarCtrl = {
         const previous = document.querySelector('#previous');
         const next = document.querySelector('#next');
         selectedMonthCopy <= monthNow ? previous.style.display = 'none' : previous.style.display = 'inline';
+        selectedMonthCopy >= monthNow+3 ? next.style.display = 'none' : next.style.display = 'inline';
         previous.addEventListener('click', function() {
             calendarDiv.innerHTML = '';
             let calendarTable = renderCalendar(createCalendar(yearNow, --selectedMonthCopy));
