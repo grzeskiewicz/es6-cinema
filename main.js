d960f7511816cc41e9c7b2debde6aae898a63458
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-10-05 22:37:05 +0200

    Editing project

--- main.js
+++ main.js
@@ -141,7 +141,7 @@ const calendarCtrl = {
         next.addEventListener('click', function() {
             console.log(selectedMonthCopy);
    
-            console.log(yearNow);
+            console.log(Number(yearNow)++);
             calendarDiv.innerHTML = '';
             let calendarTable = renderCalendar(createCalendar(yearNow, ++selectedMonthCopy));
             calendarCtrl.initListeners(calendarTable);
