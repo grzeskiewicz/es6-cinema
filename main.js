756545be8d7ac20191e5067e4ed1ff5ef27ac5be
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-10-05 23:56:24 +0200

    Editing project

--- main.js
+++ main.js
@@ -131,7 +131,7 @@ const calendarCtrl = {
         let d=Number(yearNow);
         const previous = document.querySelector('#previous');
         const next = document.querySelector('#next');
-        selectedMonthCopy <= monthNow ? previous.style.display = 'none' : previous.style.display = 'inline';
+        selectedMonthCopy <= monthNow && d < yearNow ? previous.style.display = 'none' : previous.style.display = 'inline';
         selectedMonthCopy > 12 ? next.style.display = 'none' : next.style.display = 'inline';
         if (selectedMonthCopy >=12 ) {selectedMonthCopy=0;d++}
         previous.addEventListener('click', function() {
